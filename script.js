49d2de13debd3d0c0ef6a073daa1286e65633de9
akashshawdev <aakashshaw8873@gmail.com>
2022-09-27 19:02:03 +0530

    Complete game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const score0El = document.querySelector('#score--0')
 const score1El = document.getElementById('score--1')
@@ -6,29 +6,48 @@ const current0El = document.getElementById('current--0')
 const current1El = document.getElementById('current--1')
 const player0 = document.querySelector('.player--0')
 const player1 = document.querySelector('.player--1')
+const player0NameEl = document.getElementById('name--0')
+const player1NameEl = document.getElementById('name--1')
 const diceEl = document.querySelector('.dice')
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 
-let scores = [0, 0]
-score0El.textContent = 0
-score1El.textContent = 0
-let currentScore = 0
-let activePlayer = 0
-diceEl.classList.add('hidden')
+let scores, playing, currentScore, activePlayer
 
-const switchPlayer = function() {
-    document.getElementById(`current--${activePlayer}`).textContent = 0
-    currentScore = 0
-    activePlayer = activePlayer === 0 ? 1 : 0
-    player0.classList.toggle('player--active')
-    player1.classList.toggle('player--active')
+const init = function () {
+  scores = [0, 0]
+  playing = true
+  currentScore = 0
+  activePlayer = 0
+
+  score0El.textContent = 0
+  score1El.textContent = 0
+  current1El.textContent = 0
+  current0El.textContent = 0
+
+  diceEl.classList.add('hidden')
+  player0.classList.remove('player--winner')
+  player1.classList.remove('player--winner')
+  player0.classList.add('player--active')
+  player1.classList.remove('player--active')
+  player0NameEl.textContent = 'Player 1'
+  player1NameEl.textContent = 'Player 2'
+}
+init()
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0
+  currentScore = 0
+  activePlayer = activePlayer === 0 ? 1 : 0
+  player0.classList.toggle('player--active')
+  player1.classList.toggle('player--active')
 }
 
-// Rolling dice functionality 
-btnRoll.addEventListener('click', function() {
-    // 1. Generate random dice roll 
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // 1. Generate random dice roll
     let dice = Math.trunc(Math.random() * 6) + 1
 
     // 2. Display dice roll
@@ -37,33 +56,37 @@ btnRoll.addEventListener('click', function() {
 
     // 3. Check if rolled 1
     if (dice !== 1) {
-        // Add dice to current score
-        currentScore += dice
-        // current0El.textContent = currentScore
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore
+      // Add dice to current score
+      currentScore += dice
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore
     } else {
-        // Switch player 
-        switchPlayer()
+      // Switch player
+      switchPlayer()
     }
+  }
 })
 
-btnHold.addEventListener('click', function() {
+btnHold.addEventListener('click', function () {
+  if (playing) {
     scores[activePlayer] += currentScore
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer]
 
-    if (scores[activePlayer]>=100) {
-        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
-        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
-    } 
-    else {
-        // document.getElementById(`name--${activePlayer}`).textContent = `Player ${activePlayer + 1} wins`
-        switchPlayer()
+    if (scores[activePlayer] >= 10) {
+      playing = false
+      diceEl.classList.add('hidden')
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner')
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active')
+        document.getElementById(`name--${activePlayer}`).textContent = `Player ${activePlayer + 1} wins`
+    } else {
+      switchPlayer()
     }
+  }
 })
 
-// btnNew.addEventListener('click', function() {
-//     scores = [0, 0]
-//     document.querySelectorAll('.score').textContent = 0
-// })
-
-
+btnNew.addEventListener('click', init)
